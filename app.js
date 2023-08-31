1741c8aee944f1c6c60488f077fefe171bf2b3d5
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2023-08-31 11:01:18 +0200

    ✨  variabel kod

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log("Hello world")
+console.log("Hello world");
+
+let firstname; // definera
+firstname = "Alvin"; // tilldela ett värde
+
+let lastname = "Högdal"; // definera och tilldela ett värde, deklarera
+
+let num = 42;
+
+console.log(typeof(num));
+
+const NAME = "Pog";
+
+let i;
+
+for(let i = 0; i < 10; i++){
+    const NAME = "YEET";
+    console.log(NAME);
+}
+
+console.log(i);
+
+console.log(NAME);
+
+var skola = "NTI";
+
+console.log(skola);
+
+
+let a ="Jens"; //string
+
+let b = 'Jens'; // string rekomenderas
+
+let c = 'Mitt namn är ${a}'; // string med template literals
+
+let d = 'Mitt namn är ' +  b; // konkatenering 
+
+
+let e = 12; // num 
+
+let f = 16.7; // num 
+
+
+let g = true; // boolean
+
+let h = false; // boolean
+
+
+console.log(c, e, g)
